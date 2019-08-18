d42e53c66643b865d684fcc316aa8e0044a59c1d
venkat13sv <vengi13tamilan@yahoo.com>
2019-08-18 12:33:35 +0530

    React Host App Code

--- src/App.js
+++ src/App.js
@@ -4,22 +4,22 @@ import './App.css';
 
 function App() {
   return (
+    <div>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Welcome to Angular Widget Integration into React.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+        Below is the Angular Widget App embedded in this React App.
+        </p>
       </header>
+
     </div>
+    <case-search></case-search>
+    </div>
+
   );
 }
 
