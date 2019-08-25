44acd090a07695471ccce345f978f496e48493c6
venkat13sv <vengi13tamilan@yahoo.com>
2019-08-25 22:01:21 +0530

    code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Welcome to Angular Widget Integration into React.
-        </p>
-        <p>
-        Below is the Angular Widget App embedded in this React App.
-        </p>
-      </header>
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.searchCases=this.searchCases.bind(this);
+    this.inputobj={
+      //user input to search
+      searchobj:{
+        "caseId":"",
+        "caseName":"",
+        "caseType":""
+      },
+      //user roles and additional data can be sent here
+      userobj:{
+        "role":"admin",
+        "name":"username"
+      }
+    };
 
-    </div>
-    <case-search></case-search>
-    </div>
+  }
 
-  );
+
+  searchCases(){
+    this.inputobj.searchobj= {
+      "caseId":this.caseId.value,
+      "caseName":this.caseName.value,
+      "caseType":this.caseType.value
+    };
+    console.log(this.inputobj);
+    this.forceUpdate();
+
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h1 className="App-title">Case Information Search.</h1>
+          <div className="title"></div>
+            <div className="search ">
+               <input type="text" name="caseId"  className="searchTerm" placeholder="Case Id" defaultValue="" ref={(caseId) => { this.caseId = caseId }} />
+               <input type="text" name="caseName" className="searchTerm" placeholder="Case Name" defaultValue="" ref={(caseName) => { this.caseName = caseName }}/>
+               <input type="text" name="caseType" className="searchTerm" placeholder="Case Type" defaultValue="" ref={(caseType) => { this.caseType = caseType}} />
+               <button type="submit" className="searchButton" onClick={this.searchCases}>
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
+          </header>
+          <case-search inputobj={JSON.stringify(this.inputobj)}></case-search>
+          </div>
+    );
+  }
 }
 
 export default App;
